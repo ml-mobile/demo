fb30af626c379e668bad26aad6e4c81eca1b5b51
C59406A <MongLeng.Chew@experian.com>
2020-10-02 19:26:20 +0800

    Initial

--- postgresql/sample_1/Sample Database - create objects set2.sql
+++ postgresql/sample_1/Sample Database - create objects set2.sql
@@ -67,6 +67,6 @@ CREATE TABLE sales.order_items (
 	list_price DECIMAL (10, 2) NOT NULL,
 	discount DECIMAL (4, 2) NOT NULL DEFAULT 0,
 	PRIMARY KEY (order_id, item_id),
-	FOREIGN KEY (order_id) REFERENCES sales.orders (order_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	--FOREIGN KEY (product_id) REFERENCES production.products (product_id) ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY (order_id) REFERENCES sales.orders (order_id) ON DELETE CASCADE ON UPDATE CASCADE
+	--, FOREIGN KEY (product_id) REFERENCES production.products (product_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
